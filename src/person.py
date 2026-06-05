f0069750cef24dbd8f23542025339f3848202218
wesselpierik <wessel.pierik@student.uva.nl>
2026-06-05 14:23:36 +0200

    The Person base class now saves the model

--- src/person.py
+++ src/person.py
@@ -6,6 +6,8 @@ class Person(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
+        self.model = model
+
 
 if __name__ == "__main__":
     model = base_model.BaseModel()
