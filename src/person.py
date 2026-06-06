a07a2b3d4b0ae08592c41d9e5056b90130872835
Annnvd <anna.van.dun@student.uva.nl>
2026-06-06 17:25:53 +0200

    Added random movement

--- src/person.py
+++ src/person.py
@@ -1,14 +1,27 @@
 import mesa
 import base_model
+import random
 
 
 class Person(mesa.Agent):
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, pos):
         super().__init__(unique_id, model)
 
         self.model = model
+        self.pos = pos
 
+    def move(self):
+        # Get neighbours (Moore neighbourhood) and randomly select one
+        neighbours = self.model.grid.get_neighborhood(self.pos, True)
+        selected_neighbour = random.choice(neighbours)
+        # Move agent
+        self.model.grid.move_agent(self, selected_neighbour)
 
 if __name__ == "__main__":
+    # Test the Person class with a single agent at (5,5)
     model = base_model.BaseModel()
-    person = Person(1, model)
+    model.add_agent(Person, (5, 5))
+    person = model.agents[0]
+    print(person.pos)
+    person.move()
+    print(person.pos)
