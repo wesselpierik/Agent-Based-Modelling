044cf90ea1d29898fba3be40b66193773bc4b46a
Annnvd <anna.van.dun@student.uva.nl>
2026-06-06 17:26:42 +0200

    Created method for biased movement of the thief

--- src/person.py
+++ src/person.py
@@ -17,6 +17,34 @@ class Person(mesa.Agent):
         # Move agent
         self.model.grid.move_agent(self, selected_neighbour)
 
+    def biased_move(self):
+        neighbours = self.model.grid.get_neighborhood(self.pos, True)
+
+        # Type of agent
+        agent_type = self.__class__.__name__
+
+        if agent_type == "Thief":
+            nieghbours_with_passerby = []
+
+            for neighbour in neighbours:
+                contents = self.model.grid.get_cell_list_contents([neighbour])
+
+                for content in contents:
+                    if content.__class__.__name__ == "Victim":
+                        nieghbours_with_passerby.append(neighbour)
+                
+
+            if len(nieghbours_with_passerby) > 0:
+                selected_neighbour = random.choice(nieghbours_with_passerby)
+            
+            else:
+                selected_neighbour = random.choice(neighbours)
+
+        else:
+            selected_neighbour = random.choice(neighbours)
+
+        self.model.grid.move_agent(self, selected_neighbour)
+
 if __name__ == "__main__":
     # Test the Person class with a single agent at (5,5)
     model = base_model.BaseModel()
