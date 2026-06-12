cd403f8adfb5fea89beb568a80e7a060f5234f55
Annnvd <anna.van.dun@student.uva.nl>
2026-06-12 12:02:16 +0200

    Changed server.py to fit our model

--- src/server.py
+++ src/server.py
@@ -10,33 +10,35 @@ import sys
 # Change stdout so we can ignore most prints etc.
 orig_stdout = sys.stdout
 sys.stdout = open(os.devnull, 'w')
-IPython.get_ipython().magic("run 1-mesa.ipynb")
+IPython.get_ipython().magic("base_model.py")
 sys.stdout = orig_stdout
 
 # You can change this to whatever ou want. Make sure to make the different types
 # of agents distinguishable
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle",
-                 "Color": "blue" if type(agent) is Wolf else "green",
+                 "Color": "red" if type(agent) is Thief else "green" if type(agent) is Victim else "blue",
                  "Filled": "true",
                  "Layer": 0,
                  "r": 0.5}
     return portrayal
 
 # Create a grid of 20 by 20 cells, and display it as 500 by 500 pixels
-grid = CanvasGrid(agent_portrayal, 20, 20, 500, 500)
+grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
 
 # Create a dynamic linegraph
-chart = ChartModule([{"Label": "Sheep",
+chart = ChartModule([{"Label": "Thieves",
+                      "Color": "red"},
+                      {"Label": "Victims",
                       "Color": "green"},
-                      {"Label": "Wolves",
-                      "Color": "red"}],
+                      {"Label": "Police",
+                      "Color": "blue"}],
                     data_collector_name='datacollector')
 
 # Create the server, and pass the grid and the graph
-server = ModularServer(WolfSheep,
+server = ModularServer(BaseModel,
                        [grid, chart],
-                       "WolfSheep Model",
+                       "BaseModel",
                        {})
 
 server.port = 8521
