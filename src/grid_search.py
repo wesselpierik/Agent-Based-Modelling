e8879c934914e42350b99fcb7ca84ebeaa183c9a
wesselpierik <wessel.pierik@student.uva.nl>
2026-06-27 14:03:09 +0200

    feat: Will now sweep all possible pairs

--- src/grid_search.py
+++ src/grid_search.py
@@ -51,23 +51,52 @@ problem = {
     ],
 }
 
+parameter_names = [
+    "n_police",
+    "loot",
+    "fine",
+    "police_attentiveness",
+    "police_vision_radius",
+    "thief_vision_radius",
+]
 
-def evaluate(sample):
+bounds = [
+    (1, 32),  # number of police
+    (2, 20),  # loot
+    (0.2, 7),  # fine
+    (0.1, 1.0),  # police attentiveness (float)
+    (1, 20),  # vision radius police
+    (3, 8),  # vision radius thief
+]
+
+param_bounds_dict = {name: bound for name, bound in zip(parameter_names, bounds)}
+
+baseline = {
+    "n_police": 10,
+    "loot": 5,
+    "fine": 2,
+    "police_attentiveness": 0.8,
+    "police_vision_radius": 8,
+    "thief_vision_radius": 8,
+}
+
+
+def evaluate(args):
     succesful_thieves = np.empty(replicates, dtype=np.int64)
 
+    sample, x_name, y_name = args
+
+    params = baseline.copy()
+
+    params[x_name] = sample[0]
+    params[y_name] = sample[1]
+
+    params["n_police"] = int(round(params["n_police"]))
+    params["police_vision_radius"] = int(round(params["police_vision_radius"]))
+    params["thief_vision_radius"] = int(round(params["thief_vision_radius"]))
+
     for i in range(replicates):
-
-        n_police = int(sample[0])
-        thief_vision_radius = sample[1]
-
-        model = BaseModel(
-            n_police=n_police,
-            loot=5,  # FIXED (choose baseline)
-            fine=2,  # FIXED
-            police_vision_radius=8,  # FIXED
-            thief_vision_radius=thief_vision_radius,
-            police_attentiveness=0.8,  # FIXED
-        )
+        model = BaseModel(**params)
 
         for _ in range(max_steps):
             model.step()
@@ -77,66 +106,78 @@ def evaluate(sample):
     return np.mean(succesful_thieves)
 
 
+def mesh_grid_generation(x_vals_name, y_vals_name, n_grid):
+    x_bounds = param_bounds_dict[x_vals_name]
+    y_bounds = param_bounds_dict[y_vals_name]
+
+    x_vals = np.linspace(*x_bounds, n_grid)
+    y_vals = np.linspace(*y_bounds, n_grid)
+
+    P1, P2 = np.meshgrid(x_vals, y_vals)
+
+    X = np.column_stack([P1.ravel(), P2.ravel()])
+
+    n_workers = int(os.environ.get("SLURM_CPUS_PER_TASK", mp.cpu_count()))
+
+    tasks = [(sample, x_vals_name, y_vals_name) for sample in X]
+
+    with mp.Pool(processes=n_workers) as pool:
+        chunksize = max(1, len(tasks // n_workers))
+        results_iter = pool.imap(evaluate, tasks, chunksize=chunksize)
+
+        Y = []
+
+        for y in tqdm(
+            results_iter,
+            total=len(X),
+            desc=f"Rank {rank} evaluating",
+            dynamic_ncols=True,
+        ):
+            Y.append(y)
+
+    Y_grid = np.asarray(Y).reshape(n_grid, n_grid)
+
+    plt.figure(figsize=(7, 6))
+
+    plt.contourf(P1, P2, Y_grid, levels=400, cmap="viridis")
+
+    plt.colorbar(label="Successful thefts")
+
+    plt.xlabel(x_vals_name)
+    plt.ylabel(y_vals_name)
+    plt.title("Successful thefts for different parameter pairs")
+
+    try:
+        os.mkdir("heatmaps")
+    except FileExistsError:
+        pass
+
+    try:
+        os.mkdir("raw_data")
+    except FileExistsError:
+        pass
+
+    plt.savefig(f"heatmaps/{x_vals_name} vs {y_vals_name}.png", bbox_inches="tight")
+
+    np.savez(
+        f"raw_data/{x_vals_name} {y_vals_name} {n_grid} {replicates} {max_steps}.npz"
+    )
+
+
 if __name__ == "__main__":
     # MPI setup
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    if rank == 0:
-        n_grid = 20  # resolution of sweep
+    pairs = list(combinations(parameter_names, 2))
 
-        n_police_vals = np.linspace(1, 32, n_grid)
-        thief_vision_vals = np.linspace(3, 8, n_grid)
+    local_pairs = [pair for i, pair in enumerate(pairs) if i % size == rank]
 
-        P1, P2 = np.meshgrid(n_police_vals, thief_vision_vals)
+    n_grid = 20  # resolution of sweep
 
-        X = np.column_stack([P1.ravel(), P2.ravel()])
-
-        chunks = np.array_split(X, size)
-    else:
-        chunks = None
-
-    local_X = comm.scatter(chunks, root=0)
-
-    print(f"Rank {rank}: received {len(local_X)} samples")
-
-    n_workers = int(os.environ.get("SLURM_CPUS_PER_TASK", mp.cpu_count()))
-
-    if rank == 0:
-        print(f"Using {n_workers} local workers per node")
-
-    with mp.Pool(processes=n_workers) as pool:
-        # tqdm wrapper around iterator
-        results_iter = pool.imap(evaluate, local_X)
-
-        local_Y = []
-
-        for y in tqdm(
-            results_iter,
-            total=len(local_X),
-            desc=f"Rank {rank} evaluating",
-            dynamic_ncols=True,
-        ):
-            local_Y.append(y)
-
-    all_Y = comm.gather(local_Y, root=0)
-
-    if rank == 0:
-        Y = np.concat(all_Y)
-        Y_grid = Y.reshape(n_grid, n_grid)
-
-        plt.figure(figsize=(7, 6))
-
-        plt.contourf(P1, P2, Y_grid, levels=400, cmap="viridis")
-
-        plt.colorbar(label="Successful thefts")
-
-        plt.xlabel("n_police")
-        plt.ylabel("thief_vision_radius")
-        plt.title("Successful thefts for different parameter pairs")
-
-        plt.savefig("n_police vs thief vision.png", bbox_inches="tight")
+    for x_name, y_name in local_pairs:
+        mesh_grid_generation(x_name, y_name, n_grid)
 
     comm.Barrier()
 
